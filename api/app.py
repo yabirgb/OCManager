9e599fe50118af0a6a12e35b1e24f9a41f95b92b
Yábir Garcia <yabirgb@gmail.com>
2016-12-16 13:40:13 +0100

    Closed multiline comment open

--- api/app.py
+++ api/app.py
@@ -16,3 +16,4 @@ Arrancar con: gunicorn app:app
 Ejemplo de llamada:
 
 curl -H "Authorization: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwYXNzd29yZCI6Im11Y2ggd293In0.oINalNkcc2bjo3wv_tnCyufvyTBCY6Y0wr9sQ25qacE" http://localhost:8000/hello
+"""
