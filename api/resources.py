ffc5941a78b7893aea2383faf7d7509ae02044a6
Yábir Garcia <yabirgb@gmail.com>
2017-01-06 23:57:43 +0100

    Added schema for json data validation

--- api/resources.py
+++ api/resources.py
@@ -1,31 +1,67 @@
 import falcon
 import rethinkdb as r
 import json
+from jsonschema import validate
+import datetime
 
-from db_client import *
+from models import Event, Community
 
 class Communities(object):
 
     def __init__(self):
-        self.table_name = TABLES[1]
+        self.table_name = "communities"
+
+    def _validate(self, data):
+        schema = {
+            "type":"object",
+            "description": "A representation of needed info for a community",
+            "items":{
+                "name": {"type": "string"},
+                "location": {"type": "string"},
+            },
+            "required":["name", "location"],
+        }
+
+        try:
+            validate(data, schema)
+            return True
+        except:
+            return False
 
     def add_community(self, data):
-        print(r.db(PROJECT_DB).table_list().run(db_connection))
-        r.db(PROJECT_DB).table(self.table_name).insert(data).run(db_connection)
-    
+        Community.create(c_id = str(int(datetime.datetime.now().strftime("%s")) * 1000)[:7] , name=data["name"], location = data["location"]).save()
+
     def on_post(self, req, resp):
 
         raw_json = str(req.stream.read().decode('utf-8'))
-        result = json.loads(raw_json)
-        self.add_community(result)
-        
-        resp.body = "Correctly inserted"
-        resp.status = falcon.HTTP_201
+        data = json.loads(raw_json)
+
+        if self._validate(data):
+            self.add_community(data)
+            resp.body = "Correctly inserted\n"
+            resp.status = falcon.HTTP_201
+        else:
+            resp.body = "Invalid data\n"
+            resp.status = falcon.HTTP_422
 
     def on_get(self, req, resp):
-        cursor = r.db(PROJECT_DB).table(self.table_name).run(db_connection)
-        result = {self.table_name: [i for i in cursor]}
+        result = Community.select().tuples()
         resp.body = json.dumps(result)
-        
+
+class Event(object):
+
+    def _validate(self, data):
+        schema = {
+            "type":"object",
+            "items":{
+                "name": {"type": "string"},
+                "place": {"type": "string"},
+                "date": {"type": "string", "format": "date"},
+                "summary": {"type": "string"}
+            },
+            "required":["name", "place", "date", "summary"],
+            "description": "A representation of an event"
+        }
+
 
 communities = Communities()
