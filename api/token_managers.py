4f6b5cf3301983dc85665938602191f6d6ac88d2
Yábir Garcia <yabirgb@gmail.com>
2017-02-10 21:50:37 +0100

    I have decided to change technology

--- api/token_managers.py
+++
@@ -1,7 +0,0 @@
-import jwt
-from settings import SECRET_KEY
-
-def generate_token(data):
-    return jwt.encode(data, SECRET_KEY, algorithm='HS256')
-
-print(generate_token({"user":"Yábir", "admin": True}))
