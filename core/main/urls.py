5d84a939785615ff0a4383d35c497a797bf75617
Yábir Garcia <yabirgb@gmail.com>
2017-02-11 13:10:27 +0100

    Best urls

--- core/main/urls.py
+++ core/main/urls.py
@@ -1,15 +1,18 @@
 from django.conf.urls import url, include
-from rest_framework import routers
-from api_methods import views
 from django.contrib import admin
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
+from rest_framework import routers
+from rest_framework.authtoken import views
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
+from users import urls as userUrl
+from community import urls as cUrls
+
+from users.authentication import obtain_auth_token
+
 urlpatterns = [
-    url(r'^', include(router.urls)),
+    url(r'^users/', include(userUrl)),
+    url(r'^events/', include(cUrls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^token-auth/', obtain_auth_token),
     url(r'^admin/', admin.site.urls),
 ]
