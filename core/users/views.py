497e89c1cee9e310992af0763e9892ae55875cd8
Yábir Garcia <yabirgb@gmail.com>
2017-02-11 13:12:22 +0100

    User endpoint

--- core/users/views.py
+++ core/users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework import authentication, permissions
+
+from users.serializers import UserSerializer
+from users.models import CustomUser
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    lookup_field = 'user__username'
