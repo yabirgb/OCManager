d5026c7e57daf1552ad491a98cdf6ab41c7dbfe1
Yábir Garcia <yabirgb@gmail.com>
2017-02-12 23:09:56 +0100

    Added CORS

--- core/main/settings.py
+++ core/main/settings.py
@@ -40,12 +40,13 @@ INSTALLED_APPS = [
     'community',
     'rest_framework',
     'users',
-
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -122,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL=True
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
