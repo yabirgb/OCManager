80134f64920bc31285da36c2c2811e284301e4af
yabirgb <zanklord@gmail.com>
2017-02-15 11:23:12 +0100

    Added media on dev set-up

--- core/main/urls.py
+++ core/main/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from rest_framework import routers
 from rest_framework.authtoken import views
 
@@ -15,4 +18,4 @@ urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^token-auth/', obtain_auth_token),
     url(r'^admin/', admin.site.urls),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
