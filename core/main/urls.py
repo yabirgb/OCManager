1a72618cfb980b0ccbd417e5851b38720bd74c21
Yábir <yabirgb@gmail.com>
2017-02-28 00:18:06 +0100

    Updated urls

--- core/main/urls.py
+++ core/main/urls.py
@@ -14,7 +14,7 @@ from users.authentication import obtain_auth_token
 
 urlpatterns = [
     url(r'^users/', include(userUrl)),
-    url(r'^events/', include(cUrls)),
+    url(r'^community/', include(cUrls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^token-auth/', obtain_auth_token),
     url(r'^admin/', admin.site.urls),
