baf170dd63d9fb345d4d76db93723541259cedad
Yábir Garcia <yabirgb@gmail.com>
2017-03-06 18:25:09 +0100

    Now private users only show uid

--- core/users/serializers.py
+++ core/users/serializers.py
@@ -1,20 +1,32 @@
+from collections import OrderedDict
+
 from rest_framework import serializers
 
 from .models import CustomUser
 
 class UserSerializer(serializers.ModelSerializer):
 
+    PUBLIC_FIELDS = ('uid')
+
     username = serializers.CharField(source='user.username')
     email = serializers.CharField(source='user.email')
 
     class Meta:
         model = CustomUser
-        fields = ('username', 'email', 'public', 'following')
+        fields = ('username', 'email', 'public', 'following', 'uid')
 
 
-class UserSerializerPublic(serializers.ModelSerializer):
-    #username = serializers.CharField(source='user.username')
+    def to_representation(self, obj):
+        response = super(UserSerializer, self).to_representation(obj)
 
-    class Meta:
-        model = CustomUser
-        fields = ('uid',)
+        remove = []
+        
+        if not obj.public:
+            for field in response:
+                if field not in self.PUBLIC_FIELDS:
+                    remove.append(field)
+
+        for i in remove:
+            del response[i]
+                    
+        return response
