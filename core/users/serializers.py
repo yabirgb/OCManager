4ff2c2cc4f6b01bfd9ff483afd8cf9ac1e8a95ff
Yábir <yabirgb@gmail.com>
2017-03-26 18:40:14 +0200

    Now you can register an user using the API

--- core/users/serializers.py
+++ core/users/serializers.py
@@ -3,8 +3,29 @@ from collections import OrderedDict
 from rest_framework import serializers
 
 from .models import CustomUser
+from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password")
+
+    def create(self, validated_data, **kwargs):
+        print(validated_data, **kwargs)
+        # create user
+        user = User(
+            username=validated_data['username'],
+            email = validated_data['email']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        # create profile
+
+        return user
+
+
+class ProfileSerializer(serializers.ModelSerializer):
 
     PUBLIC_FIELDS = ('uid')
 
@@ -13,20 +34,31 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CustomUser
-        fields = ('username', 'email', 'public', 'following', 'uid')
+        fields = ('username', 'email', 'public', 'following', 'uid', 'user', 'name')
 
 
     def to_representation(self, obj):
-        response = super(UserSerializer, self).to_representation(obj)
+        response = super(ProfileSerializer, self).to_representation(obj)
 
         remove = []
-        
+
         if not obj.public:
             for field in response:
                 if field not in self.PUBLIC_FIELDS:
                     remove.append(field)
-
         for i in remove:
             del response[i]
-                    
         return response
+
+    def create(self, validated_data, **kwargs):
+        print(validated_data, **kwargs)
+
+        user = CustomUser(
+            user= User.objects.get(username=validated_data["user"]['username']),
+            name = validated_data["name"]
+        )
+        user.save()
+        # create profile
+
+        return user
+        # create user
--- core/users/views.py
+++ core/users/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+
 
 from rest_framework import viewsets, generics
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework import authentication, permissions
 
-from users.serializers import UserSerializer
+from users.serializers import UserSerializer, ProfileSerializer
 from users.models import CustomUser
+
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -12,5 +17,20 @@ class UserViewSet(viewsets.ModelViewSet):
     API endpoint that allows users to be viewed or edited.
     """
     queryset = CustomUser.objects.all()
-    serializer_class = UserSerializer
+    serializer_class = ProfileSerializer
     lookup_field = 'user__username'
+
+    def create(self, request):
+        user_serializer = UserSerializer(data = request.data)
+        if user_serializer.is_valid():
+            user_obj = user_serializer.save()
+            #We create a user object using the given data
+            prof_serializer = ProfileSerializer(data = request.data)
+            #Then create a Profile for that user
+            if prof_serializer.is_valid():
+                prof_serializer.save()
+                return Response(prof_serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                return Response(prof_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
